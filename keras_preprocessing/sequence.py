d192ebd48c0e7dee1f127741cc3c959e3fdba95b
KuzMenachem <35711942+KuzMenachem@users.noreply.github.com>
2019-01-21 01:06:46 +0100

    fix line length

--- keras_preprocessing/sequence.py
+++ keras_preprocessing/sequence.py
@@ -365,7 +365,8 @@ class TimeseriesGenerator(object):
             rows = np.arange(i, min(i + self.batch_size *
                                     self.stride, self.end_index + 1), self.stride)
 
-        samples = np.array([self.data[row - self.length:row:self.sampling_rate] for row in rows])
+        samples = np.array([self.data[row - self.length:row:self.sampling_rate] 
+                           for row in rows])
         targets = np.array([self.targets[row] for row in rows])
 
         if self.reverse:
