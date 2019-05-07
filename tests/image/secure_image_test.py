85fc72ccd17448648952a5a5e237eeecef74f193
Akshay Bahadur <akshay.dominator@gmail.com>
2019-05-07 22:02:23 +0530

    fixed unit test

--- tests/image/secure_image_test.py
+++ tests/image/secure_image_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from keras_preprocessing.image import secure_image
 
+
 def test_transform_img():
     x = np.random.random((100, 100, 3))
     assert secure_image.transform_img(20, x, 100, 100).shape == (100, 100, 3)
@@ -14,7 +15,7 @@ def test_rot():
 
 def test_decrypt_img():
     path = "/some/path"
-    with pytest.raises(FileNotFoundError):
+    with pytest.raises(Exception):
         secure_image.decrypt_img(path, "password", 100, 100)
 
 
@@ -23,6 +24,7 @@ def test_trandorm():
     dest = "path/to/dest"
     with pytest.raises(FileNotFoundError):
         secure_image.transform(path, dest, count=1, block_size=39, image_x=100, image_y=100)
+
 
 def test_encrypt_directory():
     path = "/path/to/src"
