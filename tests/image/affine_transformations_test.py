Fix the axis of rotation, which has been shifted 1px from the correct location

tests/image/affine_transformations_test.py
@@ -15,16 +15,27 @@ def test_random_transforms():
 def test_deterministic_transform():
     x = np.ones((3, 3, 3))
     x_rotated = np.array([[[0., 0., 0.],
-                           [0., 0., 0.],
-                           [1., 1., 1.]],
-                          [[0., 0., 0.],
+                           [1., 1., 1.],
+                           [0., 0., 0.]],
+                          [[1., 1., 1.],
                            [1., 1., 1.],
                            [1., 1., 1.]],
                           [[0., 0., 0.],
-                           [0., 0., 0.],
-                           [1., 1., 1.]]])
+                           [1., 1., 1.],
+                           [0., 0., 0.]]])
     assert np.allclose(affine_transformations.apply_affine_transform(
         x, theta=45, channel_axis=2, fill_mode='constant'), x_rotated)
+
+
+def test_rotation_center():
+    x = np.array([[0, 1, 0],
+                  [0, 1, 0],
+                  [0, 1, 1]])[..., np.newaxis]
+    x_rotated = np.array([[0, 0, 0],
+                          [1, 1, 1],
+                          [1, 0, 0]])[..., np.newaxis]
+    assert np.allclose(affine_transformations.apply_affine_transform(
+        x, theta=90, channel_axis=2, fill_mode="constant"), x_rotated)
 
 
 def test_random_zoom():

tests/image/image_data_generator_test.py
@@ -383,14 +383,14 @@ def test_deterministic_transform():
                        x[:, ::-1, :])
     x = np.ones((3, 3, 3))
     x_rotated = np.array([[[0., 0., 0.],
-                           [0., 0., 0.],
-                           [1., 1., 1.]],
-                          [[0., 0., 0.],
+                           [1., 1., 1.],
+                           [0., 0., 0.]],
+                          [[1., 1., 1.],
                            [1., 1., 1.],
                            [1., 1., 1.]],
                           [[0., 0., 0.],
-                           [0., 0., 0.],
-                           [1., 1., 1.]]])
+                           [1., 1., 1.],
+                           [0., 0., 0.]]])
     assert np.allclose(generator.apply_transform(x, {'theta': 45}),
                        x_rotated)
 