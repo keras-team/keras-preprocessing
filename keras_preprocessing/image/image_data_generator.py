f56d9754eb7a235a4356d5bae2e6b1da96f8a602
Jake Tae <jaesungtae@gmail.com>
2020-02-24 05:12:07 +0900

    Adjusted whitespace
    Fixed indentation for readability

--- keras_preprocessing/image/image_data_generator.py
+++ keras_preprocessing/image/image_data_generator.py
@@ -324,8 +324,8 @@ class ImageDataGenerator(object):
 
         if isinstance(zoom_range, float):
             self.zoom_range = [1 - zoom_range, 1 + zoom_range]
-        elif len(zoom_range) == 2 and 
-            all(isinstance(val, float) for val in zoom_range):
+        elif len(zoom_range) == 2:
+            if all(isinstance(val, float) for val in zoom_range):
                 self.zoom_range = [zoom_range[0], zoom_range[1]]
         else:
             raise ValueError('`zoom_range` should be a float or '
