9b255a1cb9cc731f9c06a5ca6251ce781679b862
Paulo S. Costa <pauloscosta5@gmail.com>
2020-06-09 14:10:23 -0700

    Remove filtered characters at char level

--- keras_preprocessing/text.py
+++ keras_preprocessing/text.py
@@ -222,9 +222,19 @@ class Tokenizer(object):
                 a generator of strings (for memory-efficiency),
                 or a list of list of strings.
         """
+        filtered_characters = set(self.filters)
         for text in texts:
             self.document_count += 1
             if self.char_level or isinstance(text, list):
+                if isinstance(text, list):
+                    text = ["".join(char
+                                    for char in text_elem
+                                    if char not in filtered_characters)
+                            for text_elem in text]
+                else:
+                    text = "".join(char
+                                   for char in text
+                                   if char not in filtered_characters)
                 if self.lower:
                     if isinstance(text, list):
                         text = [text_elem.lower() for text_elem in text]
