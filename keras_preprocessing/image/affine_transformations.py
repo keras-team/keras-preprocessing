c748f33f3e0c7c22e693c2ec0e04de6b057e88c5
Eli Osherovich <eli.osherovich@gmail.com>
2020-10-28 15:55:38 +0200

    Set matrix center correctly
    Matrix center was set incorrectly (one off). To correct either call transform_matrix_offset_center() with h-1 ,w-1. Or to or fix the sign inside that function and continue using h and w (more intuitive).

--- keras_preprocessing/image/affine_transformations.py
+++ keras_preprocessing/image/affine_transformations.py
@@ -247,8 +247,8 @@ def random_brightness(x, brightness_range, scale=True):
 
 
 def transform_matrix_offset_center(matrix, x, y):
-    o_x = float(x) / 2 + 0.5
-    o_y = float(y) / 2 + 0.5
+    o_x = float(x) / 2 - 0.5
+    o_y = float(y) / 2 - 0.5
     offset_matrix = np.array([[1, 0, o_x], [0, 1, o_y], [0, 0, 1]])
     reset_matrix = np.array([[1, 0, -o_x], [0, 1, -o_y], [0, 0, 1]])
     transform_matrix = np.dot(np.dot(offset_matrix, matrix), reset_matrix)
