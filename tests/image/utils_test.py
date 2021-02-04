1719c526a0d6dde1ea1ca27a5980af77782f06a1
TreeKat71 <muller79924@gmail.com>
2021-02-04 23:55:55 +0800

    fix assert condition

--- tests/image/utils_test.py
+++ tests/image/utils_test.py
@@ -198,15 +198,23 @@ def test_load_img(tmpdir):
     with open(filename_grayscale_32bit, 'rb') as f:
         _path = io.BytesIO(f.read())  # io.Bytesio
     loaded_im = utils.load_img(_path, color_mode='grayscale')
-    assert loaded_im == original_grayscale_32bit
+    loaded_im_array = utils.img_to_array(loaded_im, dtype=np.int32)
+    assert loaded_im_array == original_grayscale_32bit_array
 
     _path = filename_grayscale_32bit    # str
     loaded_im = utils.load_img(_path, color_mode='grayscale')
-    assert loaded_im == original_grayscale_32bit
+    loaded_im_array = utils.img_to_array(loaded_im, dtype=np.int32)
+    assert loaded_im_array == original_grayscale_32bit_array
 
     _path = filename_grayscale_32bit.encode()  # bytes
     loaded_im = utils.load_img(_path, color_mode='grayscale')
-    assert loaded_im == original_grayscale_32bit
+    loaded_im_array = utils.img_to_array(loaded_im, dtype=np.int32)
+    assert loaded_im_array == original_grayscale_32bit_array
+
+    _path = tmpdir / 'grayscale_32bit_utils.tiff'  # Path
+    loaded_im = utils.load_img(_path, color_mode='grayscale')
+    loaded_im_array = utils.img_to_array(loaded_im, dtype=np.int32)
+    assert loaded_im_array == original_grayscale_32bit_array
 
     # Check that exception is raised if interpolation not supported.
 
